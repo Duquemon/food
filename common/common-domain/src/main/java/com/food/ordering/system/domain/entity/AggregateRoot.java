fccdbab9adafddc1904e5cd5e21b5024beef1d70
Jorge Figueroa Aqueveque <jorgeantonio.figueroaaqueveque@amaris.com>
2024-06-05 14:05:16 +0200

    Baseline porject

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.domain.entity;
+
+/**
+ * Entity marker class
+ * @param <ID>
+ */
+public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
+}
---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/event/DomainEvent.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.domain.event;
+
+/**
+ * To identify object event
+ * @param <T>
+ */
+public interface DomainEvent<T> {
+}
