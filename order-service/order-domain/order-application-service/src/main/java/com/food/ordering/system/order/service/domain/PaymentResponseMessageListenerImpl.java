cb99506fb7f4a7814ecf5d6d4fd6bc3801bf71db
Jorge Figueroa Aqueveque <jorgeantonio.figueroaaqueveque@amaris.com>
2024-06-18 16:53:45 +0200

    antes de outbox

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -1,23 +1,37 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import static com.food.ordering.system.order.service.domain.entity.Order.DELIMITER_COMMA;
 
 @Slf4j
 @Validated
 @Service
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
 
+    private final OrderPaymentSaga orderPaymentSaga;
+
+    public PaymentResponseMessageListenerImpl(OrderPaymentSaga orderPaymentSaga) {
+        this.orderPaymentSaga = orderPaymentSaga;
+    }
+
     @Override
     public void paymentCompleted(PaymentResponse paymentResponse) {
-
+        OrderPaidEvent process = orderPaymentSaga.process(paymentResponse);
+        log.info("Publishing OrderPaidEvent for order id: {}", paymentResponse.getOrderId());
+        process.fire();
     }
 
     @Override
     public void paymentCancelled(PaymentResponse paymentResponse) {
-
+        orderPaymentSaga.rollback(paymentResponse);
+        log.info("Order is rollback for order id: {} with failure message: {}",
+                paymentResponse.getOrderId(),
+                String.join(DELIMITER_COMMA, paymentResponse.getFailureMessages()));
     }
 }
